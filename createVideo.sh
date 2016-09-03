6d480fc2ed7ceeb1f7968188f39afa3b1640bbf2
LeeHD2016 <lee.herddarby@gmail.com>
2016-09-03 19:35:12 +0100

    Add files via upload

---
+++ createVideo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#v1.0 03,08,2016
+
+
+varDate=$(date -d yesterday +%Y%m%d) #set the variable as yesterday.
+
+mkdir -p /home/camera140/videos/$varDate/originals
+
+find /home/camera140/uploads -name "*$varDate*.jpg" -exec mv '{}' /home/camera140/videos/$varDate/originals \;
+cd /home/camera140/videos/$varDate/originals
+
+for name in *.jpg; do
+    convert "$name" -font courier -pointsize 20 -draw "gravity south fill black text 0,12 '$name' fill white text 1,11 '$name'" "$name";
+    echo $name;
+    done
+
+mencoder mf://*.jpg -mf w=640:h=480:fps=10:type=jpg -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o /home/camera140/videos/$varDate/$varDate.avi
+
+find /home/camera140/videos -maxdepth 1 -name "2016*" -type d -mtime +6 -exec rm -rf {} \;
